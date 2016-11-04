0f585107d8f6aa50cf6e0030f283b999860d6115
James <jakwuh@gmail.com>
2016-11-04 10:06:17 +0300

    Fix indent

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var map = {
 };
 
 module.exports = postcss.plugin('postcss-touch-callout', function() {
-	return function(root) {
+    return function(root) {
         root.walkDecls('user-select', function(decl) {
             if (decl.value in map) {
                 decl.parent.prepend({prop: '-webkit-touch-callout', value: map[decl.value]})
             }
-        })
+        });
     }
 });
